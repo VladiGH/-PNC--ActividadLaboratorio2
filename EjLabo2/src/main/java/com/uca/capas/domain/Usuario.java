845b9962876ba58a11076f2f26c3277eefad07a7
VladiGH <00103017@uca.edu.sv>
2020-04-04 23:58:41 -0600

    Lab activity done

---
+++ EjLabo2/src/main/java/com/uca/capas/domain/Usuario.java
@@ -0,0 +1,18 @@
+package com.uca.capas.domain;
+
+public class Usuario {
+	private String nombre, ocupacion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getOcupacion() {
+		return ocupacion;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setOcupacion(String ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+}
